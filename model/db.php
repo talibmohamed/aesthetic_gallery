f3c5962bb7276a4874b0c42613f6b6b65ba0ab2e
Talib Mohamed <talibmed11@gmail.com>
2024-12-12 18:43:02 +0100

    create database class

---
+++ model/db.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+class Database {
+    private $host;
+    private $user;
+    private $pass;
+    private $dbname;
+    private $dbh;
+    private $error;
+
+    public function __construct() {
+        $this->host = $_ENV['DB_HOST'];
+        $this->user = $_ENV['DB_USER'];
+        $this->pass = $_ENV['DB_PASS'];
+        $this->dbname = $_ENV['DB_NAME'];
+
+        // Set DSN
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+
+        // Create a new PDO instance
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+
+    // Method to get the PDO instance
+    public function getConnection() {
+        return $this->dbh;
+    }
+}
