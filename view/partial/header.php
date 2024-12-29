749693adaab31e682aa981215a332f4bdbc9d980
Talib Mohamed <talibmed11@gmail.com>
2024-12-29 22:20:14 +0100

    Add route for /home and enhance CSS for artwork cards and collection links

--- view/partial/header.php
+++ view/partial/header.php
@@ -31,7 +31,7 @@
             </div>
             <div class="left-lower">
                 <ul class="nav-horizontal">
-                    <li><a href="/">Home</a></li>
+                    <li><a href="home">Home</a></li>
                     <li><a href="#">Artists</a></li>
                     <li><a href="#">Artworks</a></li>
                     <li><a href="#">Contact</a></li>
